ff0601b90edce6a7d9691d813fa26cc074446edf
Chad Crowe <ccrowe@ccrowe-mn3.linkedin.biz>
2021-12-21 14:34:46 -0800

    day 13 done

--- day13/Folder.scala
+++ day13/Folder.scala
@@ -11,9 +11,9 @@ class InputReader(isTest: Boolean = true, testCase: Int = 0) {
   val points = lines.map(toPoints(_)).filter(_ != null)
   val folds = getFolds
   def getFoldInstructionStart(): Int = {
-    for(lineIndex <- 0 until lines.size){
+    for (lineIndex <- 0 until lines.size) {
       val line = lines(lineIndex)
-      if(line.contains("fold")){
+      if (line.contains("fold")) {
         return lineIndex
       }
     }
@@ -23,7 +23,7 @@ class InputReader(isTest: Boolean = true, testCase: Int = 0) {
     var instructions: List[(String, Int)] = List()
     val foldRegex = """.+([x|y])=(\d+)""".r
     val startingLine = getFoldInstructionStart()
-    for(lineIndex <- startingLine until lines.size){
+    for (lineIndex <- startingLine until lines.size) {
       val line = lines(lineIndex)
       val foldRegex(direction, index) = line
       instructions = instructions :+ (direction, index.toInt)
@@ -31,21 +31,21 @@ class InputReader(isTest: Boolean = true, testCase: Int = 0) {
     instructions
   }
   def getLines(): List[String] = {
-    
+
     val source = scala.io.Source.fromFile(pathToUse)
     val lines = source.getLines.toList
     lines
   }
   def toPoints(point: String): (Int, Int) = {
     val commanSeparated = point.split(',')
-    if(commanSeparated.size == 2){
+    if (commanSeparated.size == 2) {
       return (commanSeparated(0).toInt, commanSeparated(1).toInt)
     }
     null
   }
 }
 
-class Folder(val points: List[(Int, Int)]){
+class Folder(val points: List[(Int, Int)]) {
   val grid = createArray()
   def findArraySize(): (Int, Int) = {
     val xMax = points.map(_._1).max + 1
@@ -54,62 +54,84 @@ class Folder(val points: List[(Int, Int)]){
   }
   def createArray(): Array[Array[Int]] = {
     val (horizontalSize, verticalSize) = findArraySize()
-    var filling: Array[Array[Int]] = Array.ofDim[Int](verticalSize,horizontalSize)
-    for(point <- points){
-      val (x,y) = point
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](verticalSize, horizontalSize)
+    for (point <- points) {
+      val (x, y) = point
       filling(y)(x) = 1
     }
     filling
   }
   def printArray(): Unit = {
-    for(i <- 0 until grid.size){
-      for(j <- 0 until grid(0).size){
+    for (i <- 0 until grid.size) {
+      for (j <- 0 until grid(0).size) {
         print(grid(i)(j))
       }
       println()
     }
   }
-  
+
   //printArray()
 }
 
 object Folds {
   def verticalInverse(bottom: Array[Array[Int]]): Array[Array[Int]] = {
-    var filling: Array[Array[Int]] = Array.ofDim[Int](bottom.size, bottom(0).size)
-    for(i <- 0 until bottom.size){
-      for(j <- 0 until bottom(0).size){
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](bottom.size, bottom(0).size)
+    for (i <- 0 until bottom.size) {
+      for (j <- 0 until bottom(0).size) {
         filling(bottom.size - 1 - i)(j) = bottom(i)(j)
       }
     }
     filling
   }
   def horizontalInverse(bottom: Array[Array[Int]]): Array[Array[Int]] = {
-    var filling: Array[Array[Int]] = Array.ofDim[Int](bottom.size, bottom(0).size)
-    for(i <- 0 until bottom.size){
-      for(j <- 0 until bottom(0).size){
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](bottom.size, bottom(0).size)
+    for (i <- 0 until bottom.size) {
+      for (j <- 0 until bottom(0).size) {
         filling(i)(bottom(0).size - 1 - j) = bottom(i)(j)
       }
     }
     filling
   }
-  def addArrays(top: Array[Array[Int]], bottomFlipped: Array[Array[Int]]): Array[Array[Int]] = {
-    var filling: Array[Array[Int]] = Array.ofDim[Int](bottomFlipped.size, bottomFlipped(0).size)
-    for(i <- 0 until bottomFlipped.size){
-      for(j <- 0 until bottomFlipped(0).size){
+  def addArrays(
+      top: Array[Array[Int]],
+      bottomFlipped: Array[Array[Int]]
+  ): Array[Array[Int]] = {
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](bottomFlipped.size, bottomFlipped(0).size)
+    for (i <- 0 until bottomFlipped.size) {
+      for (j <- 0 until bottomFlipped(0).size) {
         filling(i)(j) = Math.max(bottomFlipped(i)(j), top(i)(j))
       }
     }
     filling
   }
-
+  def printArray(toPrint: Array[Array[Int]]): Unit = {
+    println()
+    for (i <- 0 until toPrint.size) {
+      for (j <- 0 until toPrint(0).size) {
+        val value = toPrint(i)(j)
+        val charToPrint = value match {
+          case 1 => "#"
+          case 0 => "."
+        }
+        print(charToPrint)
+      }
+      println()
+    }
+    println()
+  }
 }
 
-class VerticalFold(val grid: Array[Array[Int]], yFold: Int){
+class VerticalFold(val grid: Array[Array[Int]], yFold: Int) {
   def createTopFold(): Array[Array[Int]] = {
     val (verticalSize, horizontalSize) = (yFold, grid(0).size)
-    var filling: Array[Array[Int]] = Array.ofDim[Int](verticalSize, horizontalSize)
-    for(i <- 0 until filling.size){
-      for(j <- 0 until filling(0).size){
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](verticalSize, horizontalSize)
+    for (i <- 0 until filling.size) {
+      for (j <- 0 until filling(0).size) {
         filling(i)(j) = grid(i)(j)
       }
     }
@@ -118,25 +140,15 @@ class VerticalFold(val grid: Array[Array[Int]], yFold: Int){
   def createBottomFold(): Array[Array[Int]] = {
     val verticalSize = yFold
     val horizontalSize = grid(0).size
-    var filling: Array[Array[Int]] = Array.ofDim[Int](verticalSize, horizontalSize)
-    for(i <- yFold + 1 until grid.size){
-      for(j <- 0 until filling(0).size){
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](verticalSize, horizontalSize)
+    for (i <- yFold + 1 until grid.size) {
+      for (j <- 0 until filling(0).size) {
         filling(i - yFold - 1)(j) = grid(i)(j)
       }
     }
     filling
   }
-  def printArray(toPrint: Array[Array[Int]]): Unit = {
-    println()
-    for(i <- 0 until toPrint.size){
-      for(j <- 0 until toPrint(0).size){
-        print(toPrint(i)(j))
-      }
-      println()
-    }
-    println()
-  }
-
   val topFold = createTopFold()
   val bottomFold = createBottomFold()
   val bottomFlipped = Folds.verticalInverse(bottomFold)
@@ -148,9 +160,10 @@ class VerticalFold(val grid: Array[Array[Int]], yFold: Int){
 class RightFold(val grid: Array[Array[Int]], xFold: Int) {
   def createLeftFold(): Array[Array[Int]] = {
     val (verticalSize, horizontalSize) = (grid.size, xFold)
-    var filling: Array[Array[Int]] = Array.ofDim[Int](verticalSize, horizontalSize)
-    for(i <- 0 until filling.size){
-      for(j <- 0 until filling(0).size){
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](verticalSize, horizontalSize)
+    for (i <- 0 until filling.size) {
+      for (j <- 0 until filling(0).size) {
         filling(i)(j) = grid(i)(j)
       }
     }
@@ -159,9 +172,10 @@ class RightFold(val grid: Array[Array[Int]], xFold: Int) {
   def createRightFold(): Array[Array[Int]] = {
     val verticalSize = grid.size
     val horizontalSize = grid(0).size - xFold - 1
-    var filling: Array[Array[Int]] = Array.ofDim[Int](verticalSize, horizontalSize)
-    for(i <- 0 until filling.size){
-      for(j <- xFold + 1 until grid(0).size){
+    var filling: Array[Array[Int]] =
+      Array.ofDim[Int](verticalSize, horizontalSize)
+    for (i <- 0 until filling.size) {
+      for (j <- xFold + 1 until grid(0).size) {
         filling(i)(j - xFold - 1) = grid(i)(j)
       }
     }
@@ -169,9 +183,14 @@ class RightFold(val grid: Array[Array[Int]], xFold: Int) {
   }
   def printArray(toPrint: Array[Array[Int]]): Unit = {
     println()
-    for(i <- 0 until toPrint.size){
-      for(j <- 0 until toPrint(0).size){
-        print(toPrint(i)(j))
+    for (i <- 0 until toPrint.size) {
+      for (j <- 0 until toPrint(0).size) {
+        val value = toPrint(i)(j)
+        val charToPrint = value match {
+          case 1 => "#"
+          case 0 => "."
+        }
+        println(charToPrint)
       }
       println()
     }
@@ -182,16 +201,16 @@ class RightFold(val grid: Array[Array[Int]], xFold: Int) {
   val rightFlipped = Folds.horizontalInverse(rightFold)
   val horizontallyFlipped = Folds.addArrays(rightFlipped, leftFold)
   //printArray(horizontallyFlipped)
-  println("horizontal: ",horizontallyFlipped.map(y => y.sum).sum)
+  println("horizontal: ", horizontallyFlipped.map(y => y.sum).sum)
 }
 
 object Main extends App {
   val reader = new InputReader(false)
   val folder = new Folder(reader.points)
   var grid = folder.grid
-  for((direction,index) <- reader.folds){
-    println("grid size: x: ", grid(0).size, "y: ", grid.size)    
-    println("fold direction: ",direction)
+  for ((direction, index) <- reader.folds) {
+    println("grid size: x: ", grid(0).size, "y: ", grid.size)
+    println("fold direction: ", direction)
     println("fold amount: ", index)
     direction match {
       case "x" => {
@@ -205,5 +224,6 @@ object Main extends App {
       case _ => throw new Exception("this should not have happened")
     }
   }
+  Folds.printArray(grid)
 
 }
